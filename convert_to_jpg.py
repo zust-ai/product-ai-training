6f416da36dcf3c71e0c71bf5067cd27b1c2cb307
Saquiub <saquibvswild@gmail.com>
2023-02-13 15:57:24 +0530

    jpg

---
+++ convert_to_jpg.py
@@ -0,0 +1,17 @@
+
+import cv2
+
+import numpy as np
+
+base = './test_cases/inpaint/03'
+name = 'honey'
+
+path = f'{base}/{name}.png'
+
+
+img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+img = cv2.resize(img, (512, 512))
+
+
+
+cv2.imwrite(f'{base}/{name}.jpg', img)
